91a312e9d2936087a9df81901bb5f5f028d213fb
ashik-08 <ashikurrahman.8october@gmail.com>
2023-11-25 02:48:32 +0600

    added RegisterPage

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import HomePage from "../pages/HomePage/HomePage";
+import Register from "../pages/Register/Register";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-        {
-            index: true,
-            element: <HomePage />
-        }
-    ]
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
