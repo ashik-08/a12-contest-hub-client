c24d272b464f6fea8bea726fb0b0e7763505b446
ashik-08 <ashikurrahman.8october@gmail.com>
2023-11-25 01:01:41 +0600

    added MainLayout and Routes

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,9 @@
+const MainLayout = () => {
+  return (
+    <section>
+      <h2 className="text-5xl">This is main x p</h2>
+    </section>
+  );
+};
+
+export default MainLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      merriweather: ["Merriweather", "serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#3498db",
+        special: "#9b59b6",
+        head: "#2c3e50",
+        para: "#95a5a6",
+        details: "#bdc3c7",
+      },
+    },
   },
   plugins: [],
-}
+};
